209bc10ee217c91134dc7e517c7ccf65b8889093
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-25 17:27:22 +0100

    chore: rename HRPolicyContext record to HRPolicyContextRequest

--- src/main/java/org/soujava/demos/rag/HRPolicyContextRequest.java
+++ src/main/java/org/soujava/demos/rag/HRPolicyContextRequest.java
@@ -2,5 +2,5 @@ package org.soujava.demos.rag;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record HRPolicyContext(@NotBlank String context) {
+public record HRPolicyContextRequest(@NotBlank String context) {
 }
